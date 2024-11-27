6810107cff9ca543326f0dba72948fd1c19c1655
Quoc-Hwng <phamhung151020@gmail.com>
2024-11-27 09:08:02 +0700

    tailwind config

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main>
+      <h1 className="font-robert-medium text-5xl">Haha</h1>
+      <h1 className="font-circular-web text-5xl">Haha</h1>
+    </main>
+  );
+}
